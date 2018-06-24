769623e869c895de6c56674e25eca09c5a2ef2e7
Petter Quisbert <petter.quisbert@gmail.com>
2018-06-24 10:43:22 -0400

    Adding Docker scripts to automatic build

---
+++ buildAndRun.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+docker-compose up -d
+mvn clean install && docker build -t recipe-app-docker .
+docker rm -f recipe-app || true && docker run -d -p 8080:8080 --name recipe-app recipe-app-docker
